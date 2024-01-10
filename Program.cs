ad8a60da754834493ce8599ea1e8b8d4fdba2c34
Naveenn789 <149990658+Naveenn789@users.noreply.github.com>
2024-01-10 12:04:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,3 +4,10 @@ for (int i=1;i<11;i++)
 {
     Console.WriteLine($"{num}*{i}  = {(num*i)}" );
 }
+
+num = 6;
+Console.WriteLine("Table six");
+for (int i = 1; i < 11; i++)
+{
+    Console.WriteLine($"{num}*{i}  = {(num * i)}");
+}
